73da02e03ff811a326c54bd8e930bacb6903050a
Atakan Him <52455771+atakanhim@users.noreply.github.com>
2023-12-24 03:05:11 +0300

    lazy loading added day1

---
+++ Core/wardrobe.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wardrobe.Domain.Entities.Identity
+{
+    public class AppUser : IdentityUser<string>
+    {
+        public string RefreshToken { get; set; }
+        public DateTime RefreshTokenEndDate { get; set; }
+        public string NameSurname { get; set; }
+        public bool IsPremiumMember { get; set; }
+        public DateTime? PremiumMembershipExpiryDate { get; set; }
+    }
+}
---
+++ Infrastructure/wardrobe.Persistence/Contexts/wardrobeDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wardrobe.Domain.Entities.Identity;
+
+namespace wardrobe.Persistence.Contexts
+{
+    public class WardrobeDBContext : IdentityDbContext<AppUser, AppRole, string>
+    {
+   
+        public WardrobeDBContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseLazyLoadingProxies();
+            }
+        }
+    }
+}
